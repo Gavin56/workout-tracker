c4a52b270589b8b5b4f550303902a43f64087af3
Gavin56 <gavinobrien56@gmail.com>
2021-01-17 18:11:10 -0500

    Deployed to heroku and adjusted config variables to do so. Uncommented aggregate function to restore app functionality.

--- server.js
+++ server.js
@@ -15,9 +15,7 @@ app.use(express.static("public"));
 
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workoutdb", 
     { useNewUrlParser: true, 
-      useFindAndModify: false,       
-      useUnifiedTopology: true,
-      useCreateIndex: true, 
+      useFindAndModify: false
     }
   );
 
